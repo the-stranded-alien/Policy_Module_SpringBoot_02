8dff4a407ba755c50c8bc7a4c6b239edd694399f
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2021-05-31 22:22:14 +0530

    Testing Folder

---
+++ src/test/java/SpringBoot/Policy_Module_Ultimate/PolicyModuleUltimateApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBoot.Policy_Module_Ultimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyModuleUltimateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
